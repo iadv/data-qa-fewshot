f84e1a5c63209a7d15abf5e73aba95be9b5e80e1
mathewsip <53272007+mathewsip@users.noreply.github.com>
2024-07-31 18:53:36 +0100

    Update streamlit_app.py - Ian 2 files

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,28 +49,37 @@ st.markdown("")
 col1, col2 = st.columns(2)
 
 with col1:
-    uploaded_file_w = st.file_uploader("**Upload Wastage File (.csv)**", type=("csv"))
+    uploaded_file_A = st.file_uploader("**Upload File (e.g., wastage data) (.csv)**", type=("csv"))
 
 with col2:
-    uploaded_file_m = st.file_uploader("**Upload Maintenance File (.csv)**", type=("csv"))
+    uploaded_file_B = st.file_uploader("**Upload File (e.g., maintenance data) (.csv)**", type=("csv"))
 
-if uploaded_file_w and uploaded_file_m:
-    # Read the uploaded files
-    dfe = pd.read_csv(uploaded_file_w)
-    dfr = pd.read_csv(uploaded_file_m)
+# Connect to the SQLite database
+conn = sqlite3.connect('Data.db')
+
+if uploaded_file_w:
+    # Read the uploaded file
+    dfe = pd.read_csv(uploaded_file_A)
     
-    # Connect to the SQLite database
-    conn = sqlite3.connect('Data.db')
+    # Save dataframe to SQL table
+    dfe.to_sql('Data_A', conn, index=False, if_exists='replace')
     
-    # Save dataframes to SQL tables
-    dfe.to_sql('Wastage_Data', conn, index=False, if_exists='replace')
-    dfr.to_sql('Maintenance_Data', conn, index=False, if_exists='replace')
+    st.success("File A successfully uploaded and data ready for analysis.")
+
+if uploaded_file_B:
+    # Read the uploaded file
+    dfr = pd.read_csv(uploaded_file_B)
     
-    # Commit and close the connection
-    conn.commit()
-    conn.close()
+    # Save dataframe to SQL table
+    dfr.to_sql('Data_B', conn, index=False, if_exists='replace')
     
-    st.success("Files have been successfully uploaded.")
+    st.success("File B successfully uploaded and data ready for analysis.")
+
+# Commit and close the connection
+conn.commit()
+conn.close()
+    
+
 
 
 st.markdown("")
