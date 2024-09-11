6f1cd432838b051775adf4638aad276d1d3b0430
iadv <nithin.iitkgp@gmail.com>
2024-09-11 16:54:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ st.set_page_config(layout="wide")
 # stAppViewContainer
 page_bg_img = f"""
     <style>
-    [data-testid="stSidebarContent"] > .main {{
+    [data-testid="stAppViewContainer"] > .main {{
     # background-image: url("https://img.freepik.com/premium-vector/sky-blue-gradient-web-brochure-template-background-illustration-simple-plain-background_784842-323.jpg");
     background-color: #edebfc;
     background-size: cover;
